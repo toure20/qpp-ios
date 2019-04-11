806b0968f6a9627a0c70eb8b50e4d7953b838563
Abay Zhunis <bilyalov.abay@gmail.com>
2019-04-11 22:50:09 +0600

    Added profile and orders screen

--- QPP/Controllers/MyOrdersViewController.swift
+++ QPP/Controllers/MyOrdersViewController.swift
@@ -1,19 +1,15 @@
 //
-//  CartViewController.swift
+//  MyOrdersViewController.swift
 //  QPP
 //
-//  Created by Абай Жунус on 4/8/19.
+//  Created by Абай Жунус on 4/11/19.
 //  Copyright © 2019 STUDIO-X. All rights reserved.
 //
 
 import UIKit
 
-class CartViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class MyOrdersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-
-    @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,22 +17,20 @@ class CartViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.dataSource = self
         tableView.layer.cornerRadius = 12
         tableView.tableFooterView = UIView()
+        
         // Do any additional setup after loading the view.
     }
     
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 140
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CartTableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyOrdersTableViewCell", for: indexPath)
         return cell
     }
-    
-    @IBAction func nextButtonPressed(_ sender: UIButton) {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 }
