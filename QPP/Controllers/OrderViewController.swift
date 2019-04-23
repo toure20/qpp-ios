e42ba9fdcac4cb0d80cd378853822a0bd325979b
Abay Zhunis <bilyalov.abay@gmail.com>
2019-04-24 00:22:09 +0600

    order remade

---
+++ QPP/Controllers/OrderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OrderViewController.swift
+//  QPP
+//
+//  Created by Абай Жунус on 4/23/19.
+//  Copyright © 2019 STUDIO-X. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
